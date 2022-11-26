6326fb1d6a084a21978f570b1479755af05870cc
zxckeksylya <egorka.gridin@gamil.com>
2022-11-26 17:04:36 +0300

    add base control class

--- src/app/core/components/controls/test-input/test-input.component.ts
+++ src/app/core/components/controls/test-input/test-input.component.ts
@@ -1,13 +1,5 @@
-import {
-  ChangeDetectionStrategy,
-  Component,
-  Input,
-  OnDestroy,
-  OnInit,
-  Optional,
-} from '@angular/core';
-import { ControlValueAccessor, FormControl, NgControl, Validators } from '@angular/forms';
-import { Subject, takeUntil } from 'rxjs';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { BaseControl } from 'src/app/core/classes/base-control.class';
 
 @Component({
   selector: 'app-test-input',
@@ -15,55 +7,4 @@ import { Subject, takeUntil } from 'rxjs';
   styleUrls: ['./test-input.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class TestInputComponent implements ControlValueAccessor, OnInit, OnDestroy {
-  @Input() public label = '';
-
-  public formControl = new FormControl();
-  public required: boolean | undefined;
-
-  private destroy$ = new Subject<void>();
-
-  constructor(@Optional() protected ngControl: NgControl) {
-    this.ngControl.valueAccessor = this;
-  }
-
-  public ngOnInit(): void {
-    this.onInputValueChanges();
-    this.required = this.ngControl.control?.hasValidator(Validators.required);
-  }
-
-  public ngOnDestroy(): void {
-    this.destroy$.next();
-    this.destroy$.complete();
-  }
-
-  public writeValue(value: any): void {
-    this.formControl.setValue(value, { emitEvent: false });
-  }
-
-  public registerOnChange(fn: (value: any) => void): void {
-    this.onChange = fn;
-  }
-
-  public registerOnTouched(fn: () => void): void {
-    this.onTouched = fn;
-  }
-
-  public setDisabledState(isDisabled: boolean): void {
-    if (isDisabled) {
-      this.formControl.disable();
-    } else {
-      this.formControl.enable();
-    }
-  }
-
-  public onTouched: () => void = () => {};
-
-  private onInputValueChanges(): void {
-    this.formControl.valueChanges.pipe(takeUntil(this.destroy$)).subscribe(v => {
-      this.onChange(v);
-    });
-  }
-
-  private onChange: (value: any) => void = () => {};
-}
+export class TestInputComponent extends BaseControl {}
--- src/app/core/components/controls/test-number/test-number.component.ts
+++ src/app/core/components/controls/test-number/test-number.component.ts
@@ -1,13 +1,5 @@
-import {
-  ChangeDetectionStrategy,
-  Component,
-  Input,
-  OnDestroy,
-  OnInit,
-  Optional,
-} from '@angular/core';
-import { ControlValueAccessor, FormControl, NgControl, Validators } from '@angular/forms';
-import { Subject, takeUntil } from 'rxjs';
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+import { BaseControl } from '../../../classes/base-control.class';
 
 @Component({
   selector: 'app-test-number',
@@ -15,51 +7,9 @@ import { Subject, takeUntil } from 'rxjs';
   styleUrls: ['./test-number.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class TestNumberComponent implements ControlValueAccessor, OnInit, OnDestroy {
-  @Input() public label = '';
+export class TestNumberComponent extends BaseControl {
   @Input() public minValue = 0;
   @Input() public step = 1;
-
-  public formControl = new FormControl();
-  public required: boolean | undefined;
-
-  private destroy$ = new Subject<void>();
-
-  constructor(@Optional() protected ngControl: NgControl) {
-    this.ngControl.valueAccessor = this;
-  }
-
-  public ngOnInit(): void {
-    this.onInputValueChanges();
-    this.required = this.ngControl.control?.hasValidator(Validators.required);
-  }
-
-  public ngOnDestroy(): void {
-    this.destroy$.next();
-    this.destroy$.complete();
-  }
-
-  public writeValue(value: any): void {
-    this.formControl.setValue(value, { emitEvent: false });
-  }
-
-  public registerOnChange(fn: (value: any) => void): void {
-    this.onChange = fn;
-  }
-
-  public registerOnTouched(fn: () => void): void {
-    this.onTouched = fn;
-  }
-
-  public setDisabledState(isDisabled: boolean): void {
-    if (isDisabled) {
-      this.formControl.disable();
-    } else {
-      this.formControl.enable();
-    }
-  }
-
-  public onTouched: () => void = () => {};
 
   public addValue(): void {
     if (this.formControl.value < this.minValue) {
@@ -74,12 +24,4 @@ export class TestNumberComponent implements ControlValueAccessor, OnInit, OnDest
     this.formControl.setValue(this.formControl.value - this.step);
     this.onTouched();
   }
-
-  private onInputValueChanges(): void {
-    this.formControl.valueChanges.pipe(takeUntil(this.destroy$)).subscribe(v => {
-      this.onChange(v);
-    });
-  }
-
-  private onChange: (value: any) => void = () => {};
 }
