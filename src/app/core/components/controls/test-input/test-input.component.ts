4829146dcb51f5f282460982c8374d0e09a84209
zxckeksylya <egorka.gridin@gmail.com>
2022-11-26 13:39:49 +0300

    add test input

---
+++ src/app/core/components/controls/test-input/test-input.component.ts
@@ -0,0 +1,69 @@
+import {
+  ChangeDetectionStrategy,
+  Component,
+  Input,
+  OnDestroy,
+  OnInit,
+  Optional,
+} from '@angular/core';
+import { ControlValueAccessor, FormControl, NgControl, Validators } from '@angular/forms';
+import { Subject, takeUntil } from 'rxjs';
+
+@Component({
+  selector: 'app-test-input',
+  templateUrl: './test-input.component.html',
+  styleUrls: ['./test-input.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class TestInputComponent implements ControlValueAccessor, OnInit, OnDestroy {
+  @Input() public label = '';
+
+  public formControl = new FormControl();
+  public required: boolean | undefined;
+
+  private destroy$ = new Subject<void>();
+
+  constructor(@Optional() protected ngControl: NgControl) {
+    this.ngControl.valueAccessor = this;
+  }
+
+  public ngOnInit(): void {
+    this.onInputValueChanges();
+    this.required = this.ngControl.control?.hasValidator(Validators.required);
+  }
+
+  public ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
+
+  public writeValue(value: any): void {
+    this.formControl.setValue(value, { emitEvent: false });
+  }
+
+  public registerOnChange(fn: (value: any) => void): void {
+    this.onChange = fn;
+  }
+
+  public registerOnTouched(fn: () => void): void {
+    this.onTouched = fn;
+  }
+
+  public setDisabledState(isDisabled: boolean): void {
+    if (isDisabled) {
+      this.formControl.disable();
+    } else {
+      this.formControl.enable();
+    }
+  }
+
+  public onTouched: () => void = () => {};
+
+  private onInputValueChanges(): void {
+    this.formControl.valueChanges.pipe(takeUntil(this.destroy$)).subscribe(v => {
+      this.onChange(v);
+    });
+  }
+
+  private onChange: (value: any) => void = () => {};
+}
